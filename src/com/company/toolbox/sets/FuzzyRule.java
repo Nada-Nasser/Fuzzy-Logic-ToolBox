43c412f09af3b47f0d24ac671f66dc332202a052
farah afifi <36455840+farah-afifi@users.noreply.github.com>
2020-12-29 00:19:03 +0200

    minot Changes to addRules functions and Main

--- src/com/company/toolbox/sets/FuzzyRule.java
+++ src/com/company/toolbox/sets/FuzzyRule.java
@@ -4,60 +4,43 @@ import com.company.toolbox.variables.FuzzyVariable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FuzzyRule {
     String[] operators = {"&", "|", "->", "!"};
-    static String[] Parsed;
-    // len = 8
+       // len = 8
     // input_variable_name SET_NAME op input_variable_name SET_NAME -> output_variable_name SET_NAME
 
-    public FuzzyRule(String rule)//, HashMap<String,FuzzyVariable> FV)
-    {
-        Parsed = rule.split(" ");
-    }
 
+    public static HashMap<String, FuzzyVariable> getInference(ArrayList<String> fuzzyRules, HashMap<String, FuzzyVariable> inputFuzzyVariables, HashMap<String, FuzzyVariable> OutputFuzzyVariables) {
 
-    public static HashMap<String, FuzzyVariable> getInference(ArrayList<FuzzyRule> fuzzyRules, HashMap<String, FuzzyVariable> inputFuzzyVariables) {
-
-        HashMap<String, FuzzyVariable> OutputFuzzyVariables = new HashMap<>();
         for(int j = 0;j < fuzzyRules.size();j++) {
-            double OutMembership = 0;
+            String[] Parsed = fuzzyRules.get(j).split(" ");
 
+            double OutMembership = 1e7;
+            String operator;
             int i = 0;
+
             FuzzyVariable FV = inputFuzzyVariables.get(Parsed[i++]); // getting the input_variable_name
-            double MembershipSet = getMInMembership(FV, Parsed[i++]);
+            OutMembership = getInMembership(FV, Parsed[i++]);
 
-            String operator = Parsed[i++];
-
-            FuzzyVariable FV2 = inputFuzzyVariables.get(Parsed[i++]); // getting the input_variable_name
-            double MembershipSet2 = getMInMembership(FV, Parsed[i++]);
-
-            double OutputMembership = calculateMemberShip(MembershipSet, MembershipSet2, operator);
             while (!Parsed[i].equals("->")) {
                 operator = Parsed[i++];
 
                 FuzzyVariable FV3 = inputFuzzyVariables.get(Parsed[i++]); // getting the input_variable_name
-                double MembershipSet3 = getMInMembership(FV, Parsed[i++]);
+                double MembershipSet3 = getInMembership(FV3, Parsed[i++]);
 
                 OutMembership = calculateMemberShip(OutMembership, MembershipSet3, operator);
             }
 
             String OutVariableName = Parsed[++i];
-            String SetName = Parsed[i++];
+            String SetName = Parsed[++i];
 
-            if(OutputFuzzyVariables.get(OutVariableName) == null) {
-                FuzzyVariable FuzzyOut = new FuzzyVariable(OutVariableName);
-                FuzzyOut.setMembership(SetName, OutMembership);
-                OutputFuzzyVariables.put(OutVariableName,FuzzyOut);
-            }
-            else{
-                FuzzyVariable FuzzyOut = OutputFuzzyVariables.get(OutVariableName);
-                if(FuzzyOut.getMembership(SetName) == -1) {
-                    OutputFuzzyVariables.get(OutVariableName).setMembership(SetName,OutMembership);
-                }
-                else{
-                    OutputFuzzyVariables.get(OutVariableName).setMembership(SetName,Math.min(FuzzyOut.getMembership(SetName),OutMembership));
-                }
+            FuzzyVariable FuzzyOut = OutputFuzzyVariables.get(OutVariableName);
+            if (FuzzyOut.getMembershipDegree(SetName) == -1.0) {
+                OutputFuzzyVariables.get(OutVariableName).setMembershipDegree(SetName, OutMembership);
+            } else {
+                OutputFuzzyVariables.get(OutVariableName).setMembershipDegree(SetName, Math.min(FuzzyOut.getMembershipDegree(SetName), OutMembership));
             }
 
         }
@@ -73,11 +56,13 @@ public class FuzzyRule {
         else return 0.0;
     }
 
-    private static double getMInMembership(FuzzyVariable FV, String SetName){
-        if (SetName.charAt(0) == '!')
-            return  1 - FV.getMembership(SetName); //getting the input membership using SET_NAME
+    private static double getInMembership(FuzzyVariable FV, String SetName){
+        if (SetName.charAt(0) == '!') {
+
+            return 1 - FV.getMembershipDegree(SetName.substring(1)); //getting the input membership using SET_NAME
+        }
         else
-            return FV.getMembership(SetName); //getting the input membership using SET_NAME
+            return FV.getMembershipDegree(SetName); //getting the input membership using SET_NAME
     }
     /**
      *  op
